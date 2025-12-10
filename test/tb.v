36dbf27cca06d40ee6227d8ddde9ada91a94e037
gw8126 <gw8126@wayne.edu>
2025-12-09 20:35:22 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -4,7 +4,7 @@
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
-module tt_um_USERNAME_DESIGN_NAME_nand ();
+module tb ();
 
   // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
   initial begin
@@ -28,7 +28,7 @@ module tt_um_USERNAME_DESIGN_NAME_nand ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_USERNAME_DESIGN_NAME_nand user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
